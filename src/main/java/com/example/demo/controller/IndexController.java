1b096104cd7f9f0360bf67aeb486f0a1bc472557
kclaudeeager <claudekwizera003@gmail.com>
2022-01-15 15:41:05 +0200

    Changing index controller

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index() {
-        return "Welcome to translation  api.";
+        return "<body style=text-align:center><h1 >Welcome to transilation api</h1><p style=margin:10px 5px 15px 20px>Here you find the way of making transilation to any language you choose<p/></body>";
+
     }
 }
