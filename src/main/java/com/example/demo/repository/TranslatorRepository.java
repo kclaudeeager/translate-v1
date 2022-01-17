c037dd56e670cf8359ebffe5da4dfaca3711d0f8
Kwizera Claude <71407602+kclaudeeager@users.noreply.github.com>
2022-01-17 11:11:28 +0200

    Update TranslatorRepository.java

--- src/main/java/com/example/demo/repository/TranslatorRepository.java
+++ src/main/java/com/example/demo/repository/TranslatorRepository.java
@@ -18,10 +18,10 @@ public interface TranslatorRepository extends JpaRepository<Translator, Long> {
 	Translator findByvariable(String variable);
 	@Query("SELECT variable FROM Translator")
 	ArrayList<String> findAllVariable();
-@Transactional
-@Modifying
-@Query("delete from Translator t where t.variable=?1")
-Map<String,Boolean> deleteByVariable(String variable);
+//@Transactional
+//@Modifying
+//@Query("delete from Translator t where t.variable=?1")
+//Map<String,Boolean> deleteByVariable(String variable);
 //	@Transactional
 //	@Modifying
 //	@Query("update Translations t set t.language =?1 where u.variable = ?2")
